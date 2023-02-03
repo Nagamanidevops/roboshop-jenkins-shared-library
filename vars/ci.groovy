ee072d4591ed21488aca8d90ed88d34517ed265b
nagamani <nagamanichavaaws@gmail.com>
2023-02-03 06:05:04 +0000

    aaa

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -22,6 +22,7 @@ def call() {
                     script
                     {
                         common.unittest()
+                        sh env 
                     }
                 }
             }
