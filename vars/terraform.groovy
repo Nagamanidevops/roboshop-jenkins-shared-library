a45fb521e3752248edff9faf182e95df9b076283
nagamani <nagamanichavaaws@gmail.com>
2023-02-18 17:32:45 +0000

    code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,26 +1,26 @@
-def cal(){
-    pipeline{
-        
-        
-        agent{
-            node{
-                label "workstation"
-        
-        }
-        parameters {
-        string(name: 'INFRA_ENV', defaultValue: '', description: 'enter environment like dev or prod?')
+def call() {
+  pipeline {
 
-        }
-        
-        stages{
-            stage('terraform init'){
-                steps{
-                    sh "terraaform init -backend-config= env-${INFRA_ENV}/state.tfvars"
-                }
-            }
-        }
-        
-        
+    agent {
+      node {
+        label 'workstation'
+      }
     }
-}
+
+    parameters {
+      string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+    }
+
+    stages {
+
+      stage('Terraform Init') {
+        steps {
+          sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+        }
+      }
+
+    }
+
+
+    }
 }
