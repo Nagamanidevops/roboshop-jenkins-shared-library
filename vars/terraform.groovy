930a7968ca57b70ebd0003b4f8f93f9bce06a0a0
nagamani <nagamanichavaaws@gmail.com>
2023-02-23 08:28:50 +0000

    des

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -13,7 +13,7 @@ def call() {
 
     parameters {
       string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
-      choice(name: 'ACTION', choices: ['Apply', 'DESTROY'], description: 'Action')
+      choice(name: 'ACTION', choices: ['Apply', 'destroy'], description: 'Action')
 
     }
 
