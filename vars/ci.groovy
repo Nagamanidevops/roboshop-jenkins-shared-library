a48e71affbd614d43388fd61ebcde89aefe9b894
nagamani <nagamanichavaaws@gmail.com>
2023-03-23 16:21:05 +0000

    mask

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -67,6 +67,10 @@ def call() {
     env.SONAR_EXTRA_OPTS = " "
   }
 
+  if(!env.extraFiles) {
+    env.extraFiles = " "
+  }
+
   if(!env.TAG_NAME) {
     env.PUSH_CODE = "false"
   } else {
@@ -101,7 +105,7 @@ def call() {
 
       if(env.PUSH_CODE == "true") {
         stage('Upload Code to Centralized Place') {
-          echo 'Upload'
+          common.artifactPush()
         }
       }
 
