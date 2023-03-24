b68e080ee58e0d6b5ce00947db5c45c1ed87e31d
nagamani <nagamanichavaaws@gmail.com>
2023-03-24 04:53:13 +0000

    as

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -94,23 +94,27 @@ def call() {
         common.unittests()
       }
 
-      // stage('Quality Control') {
-      //   SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-      //   SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-      //   wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-      //     //sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-      //     sh "echo Sonar Scan"
-      //   }
-      // }
-      stage('place'){
-        sh 'env'
+      stage('Quality Control') {
+        // SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        // SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        // wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+        //   //sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+        //   sh "echo Sonar Scan"
+        // }
       }
+      stage('upload code to cp'){
+        
+        steps{
+          echo 'upload'
+        }
+      }
+      
 
-      // if(env.PUSH_CODE == "true") {
-      //   stage('Upload Code to Centralized Place') {
-      //     common.artifactPush()
-      //   }
-      // }
+      if(env.PUSH_CODE == "true") {
+        stage('Upload Code to Centralized Place') {
+          common.artifactPush()
+        }
+      }
 
 
     }
