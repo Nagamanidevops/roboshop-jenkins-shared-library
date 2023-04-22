63194ff7d984bf5cbc2fd1626a1926710e567d00
nagamani <nagamanichavaaws@gmail.com>
2023-04-22 10:00:32 +0000

    deploy

---
+++ vars/deploy.groovy
@@ -0,0 +1,39 @@
+def call() {
+  
+  pipeline {
+    
+    agent any 
+    
+    parameters {
+      string(name: 'APP_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+      string(name: 'COMPONENT', defaultValue: '', description: 'Enter Component name')
+      string(name: 'APP_VERSION', defaultValue: '', description: 'Enter application version to deploy')
+
+
+    }
+    options {
+      ansiColor('xterm')
+    }
+      
+    stages {
+      
+      stage('Run Deployment'){
+        
+        steps {
+          
+           sh '''
+            aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
+            
+          
+          '''
+        }
+      }
+      
+      
+    }  
+    
+    
+  }
+  
+  
+}
