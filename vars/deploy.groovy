eb764b12a1df59d778da47d92bfc1cc672b3f9af
nagamani <nagamanichavaaws@gmail.com>
2023-04-22 11:22:54 +0000

    ssh

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -14,17 +14,22 @@ def call() {
     options {
       ansiColor('xterm')
     }
+    
+     environment {
+      SSH=credentials('SSH')
+    }
       
     stages {
       
       stage('Run Deployment'){
         
-        steps {
-          
-           sh '''
+       steps {
+         
+          sh '''
             aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
-            
-          
+            aws ec2 describe-instances     --filters "Name=tag:Name,Values=${APP_ENV}-${COMPONENT}"  | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
+            ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV}
+            ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW}
           '''
         }
       }
